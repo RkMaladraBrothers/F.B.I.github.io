06660b84e993c1b52dc91a982c138e9c85c79d87
RkMaladraBrothers <95515810+RkMaladraBrothers@users.noreply.github.com>
2022-10-14 11:26:43 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -13,8 +13,8 @@ closeLightbox = () => lightbox.classList.add("hidden");
 
  function changepic(V) { document.getElementById(V).src = "IMG_20211013_212622_277.jpg"; }
  
-  function changepic1(V1) { document.getElementById(V1).src = "IMG.jpg"; }
+  function changepic1(V1) { document.getElementById(V1).src = "Img.jpg"; }
 
   function changepic01(W1) { document.getElementById(W1).src = "whatsapp-logo-app-png-4.png"; }
   
-  function changepic_1(C1) { document.getElementById(C1).src = "call.png"; }
+  function changepic_1(C1) { document.getElementById(C1).src = "Call.png"; }
